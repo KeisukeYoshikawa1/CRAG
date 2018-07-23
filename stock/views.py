ce8326cd365f90fc33de3dbb47ef64c643c07111
nanasehioki <41420743+nanasehioki@users.noreply.github.com>
2018-07-23 13:55:18 +0900

    18/07/23 13:55更新

--- stock/views.py
+++ stock/views.py
@@ -19,17 +19,35 @@ def inputpage(request):
     form = InputCrag()
     return render(request, 'inputpage.html',{'form':form})
 
+
+
+import googlemaps
+import math
+from googlemaps.places import places
+import requests
+
+
 def resultpage(request):
     form = InputCrag(request.POST)
     if form.is_valid():
         start_point_jp = form.cleaned_data.get('start_point_jp')
         input_distance_km = form.cleaned_data.get('input_distance_km')
 
+        gmaps = googlemaps.Client(key="AIzaSyB-o7p9uyxwxAcSUYtpBzhHS3jaM2JuaBw")
+        geocode_result = gmaps.geocode(start_point_jp)
+        print(geocode_result)
 
-    c = {
-        "start_point_jp": start_point_jp,
-        "input_distance_km": input_distance_km
-    }
+        print(geocode_result[0]['geometry']['location']['lat'])
+        print(geocode_result[0]['geometry']['location']['lng'])
+
+        start_point_ll = [ geocode_result[0]['geometry']['location']['lat'],geocode_result[0]['geometry']['location']['lng'] ]
+        print(start_point_ll)
+
+
+        c = {
+            'start_point_ll': start_point_ll
+            }
+
     return render(request, 'resultpage.html', c)
 
 
