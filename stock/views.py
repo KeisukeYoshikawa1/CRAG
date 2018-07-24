a2997e96d9b962688285e4e6c926fdfeebb7b529
nanasehioki <41420743+nanasehioki@users.noreply.github.com>
2018-07-24 10:33:48 +0900

    10:33有料道路のぞいたはず

--- stock/views.py
+++ stock/views.py
@@ -160,10 +160,14 @@ def resultpage(request):
         print(distance_diff_5)
 
 
+        d
+
+
         c = {
             'distance_diff_5':distance_diff_5,
             'start_point_jp':start_point_jp,
-            'start_point_ll': start_point_ll
+            'start_point_ll': start_point_ll,
+            'input_distance_km':input_distance_km,
             }
 
     return render(request, 'resultpage.html', c)
