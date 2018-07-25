08fecd7df8496496558eb3f901d7aff2c382f010
nanasehioki <41420743+nanasehioki@users.noreply.github.com>
2018-07-25 17:35:10 +0900

    17:35

--- stock/forms.py
+++ stock/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from stock.models import Crag
 
 class InputCrag(forms.Form):
-    start_point_jp = forms.CharField(label='スタート地点',widget=forms.TextInput(attrs={        'placeholder': '住所またはスポット名',    }))
-    input_distance_km = forms.IntegerField(label='距離（km）（数値のみ）' , min_value=0)
+    start_point_jp = forms.CharField(required=True,label='スタート地点',widget=forms.TextInput(attrs={        'placeholder': '住所またはスポット名', 'style':'font-size:30px;'   }))
+    input_distance_km = forms.IntegerField(required=True, label='距離（km）' , widget=forms.TextInput(attrs={        'placeholder': '数値のみ', 'style':'font-size:30px;'   }) )
